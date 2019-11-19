6cd8d7c39f45efa95b5e9c30da358692874b072f
Daniel Hahler <git@thequod.de>
2019-11-20 00:07:54 +0100

    use_scm_version: do not use --first-parent

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def main():
     setup(
         use_scm_version={
             "write_to": "src/_pytest/_version.py",
-            "git_describe_command": "git describe --dirty --tags --long --match *.* --first-parent",
+            "git_describe_command": "git describe --dirty --tags --long --match *.*",
         },
         setup_requires=["setuptools-scm", "setuptools>=40.0"],
         package_dir={"": "src"},
