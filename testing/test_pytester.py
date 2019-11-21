blueyed/pytest

commit 0118f1c081dcd1c6b6de49bc2f7fa5bc2494753b

Add _shorten_path, use it for header, _display_running (#117)

src/_pytest/pytester.py
@@ -499,9 +499,20 @@ class SysPathsSnapshot:
 
 
 def _display_running(header, *args):
-    args_str = " ".join([shlex.quote(str(x)) for x in args])
+    from _pytest.pathlib import _shorten_path
+
+    cwd = Path.cwd()
+
+    def try_rel(arg: Path) -> Path:
+        try:
+            return arg.relative_to(cwd)
+        except ValueError:
+            return arg
+
+    cmd = _shorten_path(try_rel(Path(args[0])))
+    args_str = " ".join([shlex.quote(str(x)) for x in (cmd,) + args])
     indent = " " * (len(header) - 2)
-    print("{}: {}\n{}in: {}".format(header, args_str, indent, py.path.local()))
+    print("{}: {}\n{}in: {}".format(header, args_str, indent, cwd))
 
 
 class Testdir:

src/_pytest/pathlib.py
@@ -365,3 +365,30 @@ def fnmatch_ex(pattern: str, path) -> bool:
 def parts(s: str) -> Set[str]:
     parts = s.split(sep)
     return {sep.join(parts[: i + 1]) or sep for i in range(len(parts))}
+
+
+def _shorten_path(path: Path, real_home: bool = True) -> Path:
+    if not isinstance(path, Path):
+        path = Path(path)
+    if not path.is_absolute():
+        return path
+
+    if real_home:
+        import pwd
+
+        try:
+            homedir = pwd.getpwuid(os.getuid()).pw_dir
+        except KeyError:
+            # User not in password database.
+            home = Path.home()
+        else:
+            home = Path(homedir)
+    else:
+        home = Path.home()
+
+    try:
+        rel = path.relative_to(home)
+    except ValueError:
+        return path
+    else:
+        return "~" / rel

src/_pytest/terminal.py
@@ -32,6 +32,7 @@ from _pytest.compat import shell_quote
 from _pytest.config import Config
 from _pytest.main import ExitCode
 from _pytest.main import Session
+from _pytest.pathlib import _shorten_path
 from _pytest.reports import CollectReport
 from _pytest.reports import TestReport
 
@@ -702,7 +703,7 @@ class TerminalReporter:
             or self.config.option.debug
             or getattr(self.config.option, "pastebin", None)
         ):
-            msg += " -- " + str(sys.executable)
+            msg += " -- {}".format(_shorten_path(Path(sys.executable)))
         self.write_line(msg)
         lines = self.config.hook.pytest_report_header(
             config=self.config, startdir=self.startdir
@@ -715,14 +716,16 @@ class TerminalReporter:
             self.write_line(line)
 
     def pytest_report_header(self, config):
-        line = "rootdir: %s" % config.rootdir
+        line = "rootdir: %s" % _shorten_path(Path(config.rootdir))
 
         if config.inifile:
-            line += ", inifile: " + config.rootdir.bestrelpath(config.inifile)
+            line += ", inifile: {}".format(
+                _shorten_path(Path(config.rootdir.bestrelpath(config.inifile)))
+            )
 
         testpaths = config.getini("testpaths")
         if testpaths and config.args == testpaths:
-            rel_paths = [config.rootdir.bestrelpath(x) for x in testpaths]
+            rel_paths = [str(_shorten_path(x)) for x in testpaths]
             line += ", testpaths: {}".format(", ".join(rel_paths))
         result = [line]
 

commit 73fd269a6a8db431cbfac018ffedda184cf4bca1

pytester: fix _display_running (#129)
Regressed in 0118f1c08.

src/_pytest/pytester.py
@@ -517,7 +517,7 @@ def _display_running(header, *args):
             return arg
 
     cmd = _shorten_path(try_rel(Path(args[0])))
-    args_str = " ".join([shlex.quote(str(x)) for x in (cmd,) + args])
+    args_str = " ".join([shlex.quote(str(x)) for x in (cmd,) + args[1:]])
     indent = " " * (len(header) - 2)
     print("{}: {}\n{}in: {}".format(header, args_str, indent, cwd))
 

testing/test_pytester.py
@@ -779,7 +779,7 @@ def test_spawn_interface(method, testdir, monkeypatch):
         getattr(testdir, method)(["cmd"], env={})
 
 
-def test_spawn_calls(testdir, monkeypatch):
+def test_spawn_calls(testdir, monkeypatch, capsys):
     calls = []
 
     def check_calls(*args, **kwargs):
@@ -803,6 +803,13 @@ def test_spawn_calls(testdir, monkeypatch):
     expected_env = os.environ.copy()
     expected_env.update(testdir._get_env_run_update())
     assert calls[0][1] == {"timeout": 5.0, "env": expected_env}
+
+    out, err = capsys.readouterr()
+    assert out == (
+        "=== running (spawn): cmd arg1 'arg2 with spaces'\n"
+        "                 in: {}\n".format(testdir.tmpdir)
+    )
+    assert err == ""
 
     calls.clear()
     testdir.spawn("cmd", "arg1", "arg2 with spaces", env={})