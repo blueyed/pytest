fcdcaee23b00a27144dcede2a13e9916039c4b45
Daniel Hahler <git@thequod.de>
2020-03-02 20:25:55 +0100

    fixup! Fix/adjust image references, fatal error with LaTeX

--- doc/apidoc/conf.py
+++ doc/apidoc/conf.py
@@ -157,12 +157,12 @@ html_short_title = "pytest-api-%s" % release
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "../doc/en/img/pytest1.png"
+html_logo = "../en/img/pytest1.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "../doc/en/img/favicon.png"
+html_favicon = "../en/img/favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
