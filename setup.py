aec6e65a109d468dea2379bacfa64e23637e79c3
Daniel Hahler <git@thequod.de>
2020-03-09 07:36:00 +0100

    bad

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
             ],
             "docs": [
                 "pygments-pytest>=1.1.0",
-                "sphinx @ git+https://github.com/sphinx-doc/sphinx.git@e720f0a#egg=Sphinx",
+                "sphinx @ git+https://github.com/sphinx-doc/sphinx.git@fcdeafd#egg=Sphinx",
                 "sphinxcontrib-trio",
                 "sphinx-removed-in>=0.2.0",
                 # For changelog (draft).
