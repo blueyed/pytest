fce7031ce5949714811a33dca76bb62d18ac785a
Daniel Hahler <git@thequod.de>
2020-06-30 22:49:15 +0200

    fixup! fixup! typing for diff

--- src/_pytest/python.py
+++ src/_pytest/python.py
@@ -17,8 +17,8 @@ from typing import Callable
 from typing import Dict
 from typing import Iterable
 from typing import List
-from typing import Optional
 from typing import Mapping
+from typing import Optional
 from typing import Tuple
 from typing import Union
 
