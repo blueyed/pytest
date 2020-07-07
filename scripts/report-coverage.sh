61ee35608e8956e2f828f692d3d9adb3318e4efb
Daniel Hahler <github@thequod.de>
2020-07-07 14:18:17 +0200

    ci: codecov: use fixed/pinned codecov-bash script (#376)

--- scripts/report-coverage.sh
+++ scripts/report-coverage.sh
@@ -17,5 +17,6 @@ if ! [ -f coverage.xml ]; then
 fi
 
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
-curl -S -L --connect-timeout 5 --retry 6 -s https://codecov.io/bash -o codecov-upload.sh
-bash codecov-upload.sh -Z -X fix -f coverage.xml "$@"
+curl -S -L --connect-timeout 5 --retry 6 -s -o codecov.sh \
+  https://raw.githubusercontent.com/codecov/codecov-bash/af362d8bc/codecov
+bash codecov.sh -Z -X fix -f coverage.xml "$@"
