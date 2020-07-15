af8c2777f91e7c90d5bc61eea2d9c6559c2469a0
Daniel Hahler <github@thequod.de>
2020-07-15 22:48:47 +0200

    ci: codecov.yml: codecov.notify.after_n_builds: 10 (#390)
    * ci: codecov.yml: codecov.notify.after_n_builds: 10
    * wait_for_ci: false
    * .ci/report-coverage.sh: codecov.sh: -X s3 (skip S3)

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -7,5 +7,5 @@ curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh \
   https://raw.githubusercontent.com/blueyed/codecov-bash/my-master/codecov
 
 for _ in 1 2 3 4 5; do
-  bash /tmp/codecov.sh -Z -X fix -f coverage.xml "$@" && break
+  bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage.xml "$@" && break
 done
