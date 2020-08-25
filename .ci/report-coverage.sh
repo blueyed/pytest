569747922b739d1c19506bee4987ca6d11bf22bc
Daniel Hahler <git@thequod.de>
2020-08-25 16:44:37 +0200

    .ci/report-coverage.sh: use upstream again

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -3,8 +3,7 @@
 set -ex
 
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
-curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh \
-  https://raw.githubusercontent.com/blueyed/codecov-bash/my-master/codecov
+curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://codecov.io/bash
 
 for _ in 1 2 3 4 5; do
   bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage.xml "$@" && break
