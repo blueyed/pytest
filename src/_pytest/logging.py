blueyed/pytest

commit 9090634fb3580f7795f309d5b1c01fe715c070b6

logging: use logging._checkLevel to handle (custom) levels

src/_pytest/logging.py
@@ -462,13 +462,25 @@ def get_log_level_for_setting(config: Config, *setting_names: str) -> Optional[i
             break
     else:
         return None
+    return validate_log_level(log_level, setting_name)
 
-    if isinstance(log_level, str):
-        log_level = log_level.upper()
+
+def check_level(log_level: str) -> int:
     try:
-        return int(getattr(logging, log_level, log_level))
+        rv = logging._checkLevel(log_level)  # type: int  # type: ignore[attr-defined]
     except ValueError:
-        # Python logging does not recognise this as a logging level
+        log_level_upper = log_level.upper()
+        if log_level_upper != log_level:
+            rv = logging._checkLevel(log_level_upper)  # type: ignore[attr-defined]
+        else:
+            raise
+    return rv
+
+
+def validate_log_level(log_level: str, setting_name: str) -> int:
+    try:
+        return check_level(log_level)
+    except ValueError:
         raise pytest.UsageError(
             "'{}' is not recognized as a logging level name for "
             "'{}'. Please consider passing the "

testing/logging/test_level.py
@@ -1,0 +1,60 @@
+import logging
+
+import pytest
+from _pytest.logging import validate_log_level
+from _pytest.pytester import Testdir
+
+
+def test_validate_log_level() -> None:
+    with pytest.raises(
+        pytest.UsageError,
+        match="'foo' is not recognized as a logging level name for 'setting_name'",
+    ):
+        validate_log_level("foo", "setting_name")
+
+    with pytest.raises(
+        pytest.UsageError,
+        match="'FOO' is not recognized as a logging level name for 'setting_name'",
+    ):
+        validate_log_level("FOO", "setting_name")
+
+    assert validate_log_level("debug", "setting_name") == logging.DEBUG
+
+
+def test_invalid_log_level(testdir: Testdir) -> None:
+    result = testdir.runpytest("--log-cli-level", "foo")
+    result.stderr.fnmatch_lines(
+        [
+            "ERROR: 'foo' is not recognized as a logging level name for 'log_cli_level'."
+            " Please consider passing the logging level num instead.",
+        ]
+    )
+
+
+def test_custom_log_level(testdir: Testdir) -> None:
+    testdir.makeconftest(
+        """
+        import logging
+        logging.addLevelName(42, "mylevel")
+        """
+    )
+    testdir.makepyfile(
+        """
+        import logging
+
+        def test():
+            logging.log(42, "mytestmsg")
+        """
+    )
+    result = testdir.runpytest("--log-cli-level", "mylevel")
+    result.stdout.fnmatch_lines(
+        [
+            "collected 1 item",
+            "",
+            "test_custom_log_level.py::test ",
+            "*- live log call -*",
+            "mylevel  root:test_custom_log_level.py:4 mytestmsg",
+            "PASSED * [[]100%[]]",
+            "*= 1 passed in *",
+        ]
+    )

commit 840afd0db42e979aab6b3914799d35a3f3055e99

logging: fix checking of numeric levels (e.g. `--log-cli-level=0`) (#425)
Regressed in 9090634fb.

src/_pytest/logging.py
@@ -466,6 +466,8 @@ def get_log_level_for_setting(config: Config, *setting_names: str) -> Optional[i
 
 
 def check_level(log_level: str) -> int:
+    if log_level.isnumeric():
+        return int(log_level)
     try:
         rv = logging._checkLevel(log_level)  # type: int  # type: ignore[attr-defined]
     except ValueError:

testing/logging/test_level.py
@@ -19,6 +19,8 @@ def test_validate_log_level() -> None:
         validate_log_level("FOO", "setting_name")
 
     assert validate_log_level("debug", "setting_name") == logging.DEBUG
+    assert validate_log_level("0", "setting_name") == logging.NOTSET
+    assert validate_log_level("123", "setting_name") == 123
 
 
 def test_invalid_log_level(testdir: Testdir) -> None: